653b381af84abcfa5d0499bac8d0a17d64eea263
Amit8844 <amitkrsingh1995.sup@gmail.com>
2022-08-23 00:40:44 +0530

    Turret and Player Jump Controller
    -Added Player Controller for Jump.
    -Added Turret Follow and Turret Controller.
    -Added Bullet Script.
    -Added Aim Direction.

---
+++ Paralyzed/Assets/_Project/Scripts/Characters/Player/Bullet.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Paralysed
+{
+    public class Bullet : MonoBehaviour
+    {
+        Rigidbody2D rb;
+        bool hasHit;
+
+       void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+
+        }
+        void Update()
+        {
+            HasHit();
+        }
+
+        private void HasHit()
+        {
+            if (hasHit == false)
+            {
+                float angle = Mathf.Atan2(rb.velocity.x, rb.velocity.y) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+
+        {
+            BulletEffect(other);
+
+        }
+
+        private void BulletEffect(Collision2D other)
+        {
+            if (other.gameObject.tag == "Player" || other.gameObject.tag == "Ground" || other.gameObject.tag == "Trap")
+            {
+                hasHit = true;
+                rb.velocity = Vector2.zero;
+                rb.isKinematic = true;
+                Destroy(gameObject);
+
+             }
+        }
+
+    }
+
+}
+

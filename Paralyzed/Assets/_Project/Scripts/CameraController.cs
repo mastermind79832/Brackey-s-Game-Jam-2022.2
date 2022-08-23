106537dbe8c683479a873c48f790e7b099ad27d2
Amit8844 <amitkrsingh1995.sup@gmail.com>
2022-08-23 11:02:47 +0530

    Conflicts Resolved

--- Paralyzed/Assets/_Project/Scripts/CameraController.cs
+++ Paralyzed/Assets/_Project/Scripts/CameraController.cs
@@ -1,13 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< HEAD
-using UnityEngine.Cinemachine;
 using System;
-=======
 using Cinemachine;
 using Paralysed.Core;
->>>>>>> 7c5f7b86508a319b58b9b03896a0dd19a961fe27
+
 
 namespace Paralysed.Camera
 {

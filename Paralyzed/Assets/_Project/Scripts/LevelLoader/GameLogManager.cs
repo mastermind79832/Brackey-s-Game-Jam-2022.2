6fe440e47f87bf082d2d267d57e6a3590c7e5f5a
ambrollins <gauravas079@gmail.com>
2022-08-24 22:09:12 +0530

    Level Managers done :sweat_drops:

---
+++ Paralyzed/Assets/_Project/Scripts/LevelLoader/GameLogManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public static class GameLogManager
+{
+    private static bool showLogs = false;
+
+    public static void CustomLog(object message)
+    {
+        if (!showLogs)
+        {
+            return;
+        }
+
+        Debug.Log(message);
+    }
+}

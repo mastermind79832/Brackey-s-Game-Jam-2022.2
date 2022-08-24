a8ff274c4701b6fd4e86a89fa1ae8f67013259f2
Aditya E Ajith <adityaeajith@gmail.com>
2022-08-24 12:52:36 +0530

    Created Interactable
    - Created interactable
    - Created Spirit

---
+++ Paralyzed/Assets/_Project/Scripts/Interactable/Spirit.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Paralysed.Interactable
+{
+	public class Spirit : Interactable
+	{
+		protected override void Interact()
+		{
+			// Show Message
+		}
+	}
+}

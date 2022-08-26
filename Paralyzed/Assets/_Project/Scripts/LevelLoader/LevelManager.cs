5d2c402e79f5ce1212600d632f3aea2398bb5cc7
mppavan05 <mppavan05@gmail.com>
2022-08-27 01:20:14 +0530

    Level Unlocked

---
+++ Paralyzed/Assets/_Project/Scripts/LevelLoader/LevelManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
+using UnityEngine.SceneManagement;
+
+namespace Paralysed.Level
+{
+    public class LevelManager : MonoBehaviour
+    {
+        private int levelsUnlocked;
+
+        public Button[] buttons;
+        // Start is called before the first frame update
+        void Start()
+        {
+            levelsUnlocked = PlayerPrefs.GetInt("levelsUnlocked", 1);
+
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].interactable = false;
+            }
+            
+            for (int i = 0; i < levelsUnlocked; i++)
+            {
+                buttons[i].interactable = true;
+            }
+
+        }
+
+        public void LoadLevel(int levelIndex)
+        {
+            SceneManager.LoadScene(levelIndex);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}

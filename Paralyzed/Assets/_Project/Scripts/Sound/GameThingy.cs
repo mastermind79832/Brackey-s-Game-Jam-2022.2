413cb308f1dea218aaa9429a786e82fe6df83ed5
ambrollins <gauravas079@gmail.com>
2022-08-27 22:31:06 +0530

    sound_manager_40%

---
+++ Paralyzed/Assets/_Project/Scripts/Sound/GameThingy.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+public class GameThingy : MonoBehaviour
+{
+    public AudioClip BattleMusic;
+    void Start() {
+        SoundManager.Instance.PlayMusic(BattleMusic);
+    }
+}
